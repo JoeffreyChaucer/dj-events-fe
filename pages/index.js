a45d8102efd84718a58fa4be51c5e743db1c9d42
JoeffreyChaucer <joeffrey.chaucer.paitan@gmail.com>
2021-07-16 15:13:14 +0800

    Showcase component + Event API routes (temp)

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import Layout from '@/components/Layout';
 
 export default function HomePage() {
-  return <></>;
+  return (
+    <>
+      <Layout>
+        <h1>Home</h1>
+      </Layout>
+    </>
+  );
 }
