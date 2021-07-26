4fded3523952ae35fafd22863e7f1ebe201f80c5
JoeffreyChaucer <joeffrey.chaucer.paitan@gmail.com>
2021-07-26 14:50:33 +0800

    fix typo

--- pages/account/register.js
+++ pages/account/register.js
@@ -72,7 +72,7 @@ export default function RegisterPage() {
               onChange={(e) => setPasswordConfirm(e.target.value)}
             />
           </div>
-          <input type='submit' value='Login' className='btn' />
+          <input type='submit' value='Register' className='btn' />
         </form>
 
         <p>
